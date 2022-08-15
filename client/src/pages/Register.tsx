4ef57953fe9730f33a327652d1c327de5ab39f72
MoraesFausto <faustoalexandre860@gmail.com>
2022-08-15 15:22:07 -0300

    Portal Geoeste v0.1

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -3,7 +3,6 @@ import { RegisterForm } from "../components/Forms/RegisterForm"
 export const Register: React.FC = () =>{
     return(
         <div>
-            <h2> Cadastre-se agora mesmo! </h2>
             <RegisterForm/>
         </div>
     )
