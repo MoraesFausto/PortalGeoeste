332aa32157bd727b880d79ac31039cd8e2910f21
MoraesFausto <faustoalexandre860@gmail.com>
2022-08-18 22:28:27 -0300

    Portal Geoeste v0.1

--- client/src/components/Logo/Logo.tsx
+++ client/src/components/Logo/Logo.tsx
@@ -3,7 +3,7 @@ export const Logo = () => {
     <div>
         <img style={{"width":"40vw", "marginTop":"-1.5vw"}}
         src={require('../../assets/compass_with_p_blue_no_word.png')}/>
-        <div style={{"marginLeft":"60%", "marginTop":"-10vw", "fontSize":"1.5vw", "textAlign": "center"}}
+        <div style={{"marginLeft":"60%", "marginTop":"-15vw", "fontSize":"1.5vw", "textAlign": "center"}}
         >
         <h3> Portal de Dados Ambientais  e Agropecuários </h3> 
             <h3>da </h3>
