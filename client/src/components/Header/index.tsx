4409393e2536bf8b2db78f1572c3bc77e69be55d
MoraesFausto <faustoalexandre860@gmail.com>
2022-10-14 18:37:24 -0300

    Removendo conflito com styled-components na aplicação cliente

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -1,14 +1,9 @@
-import React, {useContext} from "react";
 import { Container } from "./style";
-import Switch  from "react-switch";
-import { ThemeContext } from "styled-components"; 
-import {shade} from "polished"
-import { Link } from "react-router-dom";
-import { Nav } from "react-bootstrap";
+
 
 
 const Header: React.FC = () =>{
-    const {colors, title} = useContext(ThemeContext);
+
     return(
         <Container>
             Portal Geoeste
