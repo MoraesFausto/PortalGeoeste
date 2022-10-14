87a1a712c330fbd55684709c70c6d5c2e13c938c
MoraesFausto <faustoalexandre860@gmail.com>
2022-10-14 18:34:22 -0300

    Removendo conflito com styled-components na aplicação cliente

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -6,28 +6,12 @@ import {shade} from "polished"
 import { Link } from "react-router-dom";
 import { Nav } from "react-bootstrap";
 
-interface Props {
-    toggleTheme(): void; 
-}
 
-const Header: React.FC<Props> = ({toggleTheme}) =>{
+const Header: React.FC = () =>{
     const {colors, title} = useContext(ThemeContext);
     return(
         <Container>
             Portal Geoeste
-
-            <Switch
-                onChange={toggleTheme}
-                checked={title === 'dark'}
-                checkedIcon={false}
-                uncheckedIcon={false}
-                height={10}
-                width={40}
-                handleDiameter={18}
-                offColor={shade(.15, colors.primary)}
-                onColor={colors.secondary}
-            />
-
         </Container>
     );
 };
